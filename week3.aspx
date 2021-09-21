56540bd175ca8da55b38e88855bb60eff8b0954c
michaelbycycle <michaeljwade2@gmail.com>
2021-09-20 20:19:33 -0600

    Add files via upload

---
+++ week3.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="week3.aspx.cs" Inherits="Sept9.WebForm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="First Name "></asp:Label>
+            <asp:TextBox ID="txtFName" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <br />
+
+            <asp:Label ID="Label2" runat="server" Text="Last Name "></asp:Label>
+            <asp:TextBox ID="txtLName" runat="server"></asp:TextBox>
+             <br />
+
+            <asp:Label ID="Label3" runat="server" Text="Password "></asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+            <br /><br />
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </div>
+       
+    </form>
+</body>
+</html>
---
+++ week3.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sept9
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string txtFirstName = Request.QueryString["txtFirstName"];
+           
+
+            //get the cookie out!
+            HttpCookie myCookie = Request.Cookies["lastName_cook"];
+            string txtLastName = myCookie.Value;
+
+            string hidden_key = Session["hidden_key"].ToString();  
+
+            Response.Write(txtFirstName + " " + txtLastName + " : " + hidden_key);
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //retrieve first name from textbox control
+            string txtFirstName = txtFName.Text;
+
+            Response.Redirect("week3.aspx?txtFirstName=" + txtFirstName);
+
+            //retrieve last name from textbox control
+            string txtLastName = txtLName.Text;
+
+            // create a cookie object
+            HttpCookie myCookie = new HttpCookie("lastName_cook");
+            //add the value to the cookie
+            myCookie.Value = txtLastName;
+            //add the cookie to the cookies
+            Response.Cookies.Add(myCookie);
+
+            
+            string hidden_key = txtPassword.Text;
+
+            //Response.Write(hidden_key);
+
+            //add a session
+            Session.Add("hidden_key", hidden_key);
+        }
+    }
+}
